034844b598d866cd4d5daea76b1bd676bf871eb5
fehdamara <155977715+fehdamara@users.noreply.github.com>
2025-09-08 00:02:46 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import './styles/global.css';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
