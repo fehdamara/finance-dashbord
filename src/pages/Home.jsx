608b5f68bab09c632b9d5847ec167ef810cca058
fehdamara <155977715+fehdamara@users.noreply.github.com>
2025-09-08 00:00:40 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
+import background from "../assets/background.jpg";
+
 export default function Home() {
     return (
-      <div className="text-center">
-        <h1>Welcome to the Finance Dashboard</h1>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        color: "white"
+      }}
+    >
+      <h1 className="text-center pt-5">Benvenuto nel Finance Dashboard</h1>
+              
         <p>Track your spending and manage your finances smartly.</p>
-      </div>
+
+    </div>
     );
   }
+
