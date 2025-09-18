a16bf58b7d95024085eab0ab18a11dbd52ecc5a3
fehdamara <155977715+fehdamara@users.noreply.github.com>
2025-09-18 16:42:29 +0200

    Update Header.jsx
    comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
+// Componente che mostra una barra di navigazione in alto con link alla home, dashboard, transazioni e pannello admin
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
+      {/* Link al logo o nome dell'app */}
       <Link className="navbar-brand" to="/">
         Finance Dashboard
       </Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto">
+          {/* Link a ciascuna sezione dell'app */}
           <li className="nav-item">
             <Link className="nav-link" to="/dashboard">Dashboard</Link>
           </li>
@@ -23,3 +26,4 @@ export default function Header() {
     </nav>
   );
 }
+
