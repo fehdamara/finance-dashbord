a0a7db172061d461219a2c335b488d8768478c41
fehdamara <155977715+fehdamara@users.noreply.github.com>
2025-09-18 17:32:33 +0200

    Update main.jsx
    commented

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
+// Punto di ingresso dell'applicazione React
+// Qui viene creata e avviata l'app React collegandola a Redux e React Router
+
+
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import './styles/global.css';
+import ReactDOM from "react-dom/client"; // API moderna per creare il root React 18+
+import App from "./App"; // Componente principale
+import { BrowserRouter } from "react-router-dom"; // Fornisce il supporto al routing
+import { Provider } from "react-redux"; // Fornisce il Redux store a tutta l'app
+import store from "./redux/store"; // Il Redux store configurato
+import "bootstrap/dist/css/bootstrap.min.css"; // Importa Bootstrap
+import "./styles/global.css"; // Importa gli stili globali definiti in CSS
 
 
+// Crea il root dell'app React e renderizza l'app intera nel div con id 'root'
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+<Provider store={store}> {/* Rende disponibile lo store Redux */}
+<BrowserRouter> {/* Attiva il routing nell'app */}
+<App /> {/* Componente principale */}
+</BrowserRouter>
+</Provider>
 );
